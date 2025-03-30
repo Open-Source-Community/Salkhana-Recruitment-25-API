850a0877d51ef07c630f97c577808d6b0927be3c
meefr <meefr25@gmail.com>
2025-03-30 09:58:45 +0200

    adding api files

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const {
+  stringToArr,
+  getQuestions,
+  checkFileName,
+  generateMarkdownFile,
+  generateRandomQuestions,
+  validateQuestions,
+} = require("./utility");
+
+app.get("/download", (req, res) => {
+  let { beginner, intermediate, advanced, name, interviewer_name, random } =
+    req.query;
+  let q = {};
+
+  if (!name) return res.status({ msg: "Add interviewee name!" });
+  name = name.replace(" ", "_");
+  const checkName = checkFileName(name);
+  if (checkName) {
+    res.send({
+      msg: "Interviewee did an interview before, make sure to enter new interviewee name",
+    });
+  }
+  if (random) {
+    x = generateRandomQuestions();
+
+    beginner = x.beginner;
+    intermediate = x.intermediate;
+    advanced = x.advanced;
+  }
+  const checkQuestions = validateQuestions(beginner, intermediate, advanced);
+
+  beginner = stringToArr(beginner);
+  intermediate = stringToArr(intermediate);
+  advanced = stringToArr(advanced);
+  if (!checkQuestions)
+    return res.send({
+      msg: "Invalid Questions Criteria !",
+    });
+  q["Beginner"] = getQuestions("Beginner", beginner);
+  q["Intermediate"] = getQuestions("Intermediate", intermediate);
+  q["Advanced"] = getQuestions("Advanced", advanced);
+
+  // Generate markdown file
+  let filePath = generateMarkdownFile(q, name, interviewer_name);
+  filePath = filePath.replace(/\\/g, "/");
+  return res.send({ status: 200, filePath: filePath, msg: q });
+});
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
---
+++ utility.js
@@ -0,0 +1,157 @@
+const fs = require("fs");
+const path = require("path");
+const data = JSON.parse(fs.readFileSync("./questions.json", "utf8"));
+const stringToArr = (str) => {
+  if (str) return str.split(" ");
+  return [];
+};
+const getQuestions = (level, numbers) => {
+  return data[level]
+    .map((item, index) => ({ ...item, originalIndex: index }))
+    .filter((item) =>
+      numbers.some((num) => num === String(item.originalIndex + 1))
+    )
+    .map((item) => ({ ...item, index: item.originalIndex + 1 }));
+};
+const convertToMarkdown = (questions) => {
+  let markdownContent = "";
+  questions.forEach((q) => {
+    markdownContent += `\n\n${q.index}. **Question:** \n${q.question}\n\n`;
+    if (q.code) {
+      markdownContent += "```javascript\n" + q.code + "\n```\n";
+    }
+    markdownContent += "</details>\n\n\n";
+  });
+  return markdownContent;
+};
+const checkFileName = (name) => {
+  // Define the path where the .md file will be saved
+  const dirPath = path.join(__dirname, "/data");
+  const filePath = path.join(dirPath, `${name}.md`);
+  // Ensure the 'data' directory exists
+  return fs.existsSync(filePath);
+};
+const generateMarkdownFile = (data, name, interviewer_name) => {
+  let markdown = `## Name: \`${name.replace(
+    "_",
+    " "
+  )}\`\n## Interviewer name: \`${interviewer_name}\`\n## Results: \`\`\n\n`;
+
+  Object.keys(data).forEach((level) => {
+    markdown += `\n# ${level} Questions\n\n`;
+    markdown += convertToMarkdown(data[level]);
+  });
+
+  // Define the path where the .md file will be saved
+  const dirPath = path.join(__dirname, "/data");
+
+  // Ensure the 'data' directory exists
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+
+  const filePath = path.join(dirPath, `${name}.md`);
+
+  try {
+    fs.writeFileSync(filePath, markdown);
+    console.log("Markdown file written successfully!");
+  } catch (error) {
+    console.error("Error writing file:", error);
+  }
+  return filePath; // Return the file path for download
+};
+function getRandomNumbers(range) {
+  const [start, end] = range;
+  const numbers = [];
+
+  while (numbers.length < end - start + 1) {
+    const randomNum = Math.floor(Math.random() * (end - start + 1)) + start;
+    if (!numbers.includes(randomNum)) {
+      numbers.push(randomNum);
+    }
+  }
+
+  return numbers;
+}
+
+function generateRandomQuestions() {
+  // Beginner
+  const beginner = [
+    ...getRandomNumbers([1, 13]).slice(0, 2), // Choose 2 from [1-13]
+    ...getRandomNumbers([14, 15]).slice(0, 1), // Choose 1 from [14, 15]
+    ...getRandomNumbers([16, 18]).slice(0, 1), // Choose 1 from [16, 18]
+  ];
+
+  // Intermediate
+  const intermediate = [
+    ...getRandomNumbers([1, 14]).slice(0, 2), // Choose 2 from [1-14]
+    ...getRandomNumbers([15, 17]).slice(0, 1), // Choose 1 from [15, 17]
+    ...getRandomNumbers([18, 19]).slice(0, 1), // Choose 1 from [18, 19]
+    ...getRandomNumbers([20, 21]).slice(0, 1), // Choose 1 from [20, 21]
+  ];
+
+  // Advanced
+  const advanced = [
+    ...getRandomNumbers([1, 12]).slice(0, 1), // Choose 1 from [1-12]
+    ...getRandomNumbers([13, 15]).slice(0, 1), // Choose 1 from [13, 15]
+    ...getRandomNumbers([16, 18]).slice(0, 1), // Choose 1 from [16, 18]
+    ...getRandomNumbers([19, 20]).slice(0, 1), // Choose 1 from [19, 20]
+    ...getRandomNumbers([21, 23]).slice(0, 1), // Choose 1 from [21, 23]
+    ...getRandomNumbers([24, 26]).slice(0, 1), // Choose 1 from [24, 26]
+  ];
+
+  // Join the numbers into strings separated by spaces
+  const beginnerString = beginner.join(" ");
+  const intermediateString = intermediate.join(" ");
+  const advancedString = advanced.join(" ");
+
+  return {
+    beginner: beginnerString,
+    intermediate: intermediateString,
+    advanced: advancedString,
+  };
+}
+
+function validateQuestions(beginnerStr, intermediateStr, advancedStr) {
+  const parseNumbers = (str) => str.split(" ").map(Number);
+
+  const countInRange = (numbers, min, max) =>
+    numbers.filter((num) => num >= min && num <= max).length;
+
+  const validateBeginner = (numbers) =>
+    countInRange(numbers, 1, 13) >= 2 &&
+    countInRange(numbers, 14, 15) >= 1 &&
+    countInRange(numbers, 16, 18) >= 1;
+
+  const validateIntermediate = (numbers) =>
+    countInRange(numbers, 1, 14) >= 2 &&
+    countInRange(numbers, 15, 17) >= 1 &&
+    countInRange(numbers, 18, 19) >= 1 &&
+    countInRange(numbers, 20, 21) >= 1;
+
+  const validateAdvanced = (numbers) =>
+    countInRange(numbers, 1, 12) >= 1 &&
+    countInRange(numbers, 13, 15) >= 1 &&
+    countInRange(numbers, 16, 18) >= 1 &&
+    countInRange(numbers, 19, 20) >= 1 &&
+    countInRange(numbers, 21, 23) >= 1 &&
+    countInRange(numbers, 24, 26) >= 1;
+
+  const beginner = parseNumbers(beginnerStr);
+  const intermediate = parseNumbers(intermediateStr);
+  const advanced = parseNumbers(advancedStr);
+
+  const isValidBeginner = validateBeginner(beginner);
+  const isValidIntermediate = validateIntermediate(intermediate);
+  const isValidAdvanced = validateAdvanced(advanced);  
+  return isValidBeginner && isValidIntermediate && isValidAdvanced;
+}
+
+module.exports = {
+  stringToArr,
+  getQuestions,
+  checkFileName,
+  generateMarkdownFile,
+  generateRandomQuestions,
+  validateQuestions,
+};
